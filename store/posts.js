fab86a073417d94c34f27a2108920030e730437d
Strangebrewer <BKAShambala@gmail.com>
2020-01-15 00:40:17 -0700

    first commit - course completed

---
+++ store/posts.js
@@ -0,0 +1,20 @@
+export const state = () => ({
+   all: [
+      {
+         id: "balut",
+         title: "What is Balut?",
+         content:
+            "If someone placed balut on your plate, you might think they were serving you a hardboiled egg. That is, until you cracked it open and a fully intact shitbird fell out."
+      },
+      {
+         id: "whereIsIt",
+         title: "Where is the sign in button?",
+         content: "It's the beat to Funkytown!"
+      },
+      {
+         id: "how",
+         title: "How can I fix this problem?",
+         content: "It's not such a big deal, you know."
+      }
+   ]
+});
